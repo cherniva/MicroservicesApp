5311f45cf98a693fb89c60a8a7b2e0326ccc288a
Ivan Chernikov <chernikoviivan@gmail.com>
2024-01-30 12:23:56 +0100

    delete folders from git

--- order-service/src/main/java/com/cherniva/orderservice/repository/OrderLineItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cherniva.orderservice.repository;
-
-import com.cherniva.orderservice.model.OrderLineItems;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderLineItemsRepository extends CrudRepository<OrderLineItems, Long> {
-}
--- order-service/src/main/java/com/cherniva/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cherniva.orderservice.repository;
-
-import com.cherniva.orderservice.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
