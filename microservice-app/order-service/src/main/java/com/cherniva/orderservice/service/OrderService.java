00d4201106b8dce1b32b56a2bb2550aba99a05de
Ivan Chernikov <chernikoviivan@gmail.com>
2024-01-30 16:33:18 +0100

    Adopt all services to use eureka discovery server

--- microservice-app/order-service/src/main/java/com/cherniva/orderservice/service/OrderService.java
+++ microservice-app/order-service/src/main/java/com/cherniva/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -45,8 +45,8 @@ public class OrderService {
                 .toList();
 
         // check if order line items are in inventory stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
