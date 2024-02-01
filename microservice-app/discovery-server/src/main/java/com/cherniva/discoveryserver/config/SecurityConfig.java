fb54a12dce9dfb533e2a6654474d12a1e838da44
Ivan Chernikov <chernikoviivan@gmail.com>
2024-02-01 16:33:18 +0100

    Implement api gateway so now user does not have to know uri of all service, he can access them through api. Also secure application using spring-security oauth2 and keycloak

---
+++ microservice-app/discovery-server/src/main/java/com/cherniva/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cherniva.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${app.eureka.username}")
+    private String username;
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()) //for test purposes only
+                .withUser(username).password(password)
+                .authorities("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
